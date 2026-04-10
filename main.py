9b6cf091dbcc869ec4101433593defa8a398c52d
Lingraj Jamkhandi <lingraj43@gmail.com>
2026-04-10 19:46:13 +0530

    added analysis logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
 import pandas as pd
+from services import analyze_dataframe
 
 app = FastAPI()
 
@@ -9,3 +10,37 @@ def analyze_csv_file(file: UploadFile = File(...)):
 
     if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Only CSV files are allowed")
+    
+    try:
+        df = pd.read_csv(file.file)
+    
+    except Exception:
+
+        raise HTTPException(status_code=400, detail="Invalid CSV file")
+    
+    return analyze_dataframe(df)
+    
+def analyze_dataframe(df):
+    
+    rows, cols = df.shape
+
+    column_names = list(df.columns)
+
+    missing_values = df.isnull().sum().to_dict()
+
+    numeric_stats = {}
+
+    for col in df.select_dtypes(include=['number']).columns:
+        numeric_stats[col] = {
+            "mean": float(df[col].mean()),
+            "min": float(df[col].min()),
+            "max": float(df[col].max())
+        }
+
+    return {
+    "rows": rows,
+    "columns": cols,
+    "column_names": column_names,
+    "missing_values": missing_values,
+    "statistics": numeric_stats
+    }
